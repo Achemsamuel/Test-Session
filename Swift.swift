ebed7c49335d048a161fcac2f4a5544d7e4b1d31
Achem Samuel <Achemsamuel@gmail.com>
2020-12-11 07:59:39 +0100

    Update Swift.swift

--- Swift.swift
+++ Swift.swift
@@ -3,27 +3,7 @@ import Foundation
 public typealias Parameters = [String:Any]
 final class SwiftClass {
 
-    func checkThis() {
-        print("this is me here working...")
-        
+    func check<T: Decodable>(url: String, params: Parameters, returning objectType: T.Type, completion: ((Result<T, Error>) -> Void)? = nil) {
+           print("this is me here working...")
     }
-
-    func get<T: Decodable>(url: String, params: Parameters, returning objectType: T.Type, completion: ((Result<T, Error>) -> Void)? = nil) {
-
-    }
-}
-
-struct Specs: Codable {
-
-    var gender: Gender?
-    var height: Double?
-    var isGoodLooking: Bool?
-    var earns6Figures: Bool?
-    var isSmart: Bool?
-    var SCC: Bool?
-}
-
-enum Gender: String, Codable {
-    case male
-    case female
 }
