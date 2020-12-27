dfc1282dc99a14b275fbcc380d52f7a8527bfdac
Achem Samuel <achemsamuel@gmail.com>
2020-12-27 20:33:28 +0100

    add sources and tests

---
+++ Sources/Test-Session/Fits.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Achem Samuel on 12/27/20.
+//
+
+import Foundation
+
+final class Fits {
+    
+    func isNumberGreaterThanZero(_ number: Int) -> Bool {
+        return number > 0 ? true : false
+    }
+}
